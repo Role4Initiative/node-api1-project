dc442169e4585160d5f1b8d7dd29ae7d01a7fc4c
Benjamin Rusinowski <88050008+Role4Initiative@users.noreply.github.com>
2022-08-09 01:06:46 -0400

    Add, commit, and push for submit.

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const server = require('./api/server');
 const port = 9000;
 
 // START YOUR SERVER HERE
+// Code Start
+server.listen(port, () => {
+    console.log(`Server is listening on ${port}!`);
+});
